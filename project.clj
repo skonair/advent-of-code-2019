165be8cb81261c8f128341f6f56fa7741b07a76a
Sascha Koch <sascha.koch@web.de>
2019-12-01 14:41:27 +0100

    Solution for day1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject advent-of-code-2019 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Clojure solutions for the Advent of Code 2019"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
---
+++ src/advent_of_code_2019/day01.clj
@@ -0,0 +1,22 @@
+(ns advent-of-code-2019.day01)
+(require '[clojure.string :as str])
+
+(defn required-fuel
+  [m]
+  (- (quot m 3) 2))
+
+(defn fuel [ms]
+  (apply + (map required-fuel ms)))
+
+(defn required-fuel-fuel
+  [m]
+  (loop [r m
+         f 0]
+    (if (zero? r)
+      f
+      (let [rf (max 0 (required-fuel r))]
+        (recur rf (+ f rf))))))
+
+(defn fuel-fuel [ms]
+  (apply + (map required-fuel-fuel ms)))
+
