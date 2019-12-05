b35f0984364dca0cc970e721287c8b619566d9f7
Sascha Koch <sascha.koch@web.de>
2019-12-05 18:54:36 +0100

    day05 part 1 solution

--- src/advent_of_code_2019/day05.clj
+++ src/advent_of_code_2019/day05.clj
@@ -4,16 +4,18 @@
 (defn- exp [x n]
   (reduce * (repeat n x)))
 
-(defn- get-next-input [ & ps] 1)
+(defn- get-next-input [ & ps] 
+  (println "< < < < INPUT")
+  1)
 
-(defn- print-value [p & ps] (println p)) 
+(defn- print-value [p & ps] (println "> > > > OUTPUT: " p)) 
 
-(defn- modify-is [is ps value]
-  (println "modify-is " is ps value)
-  (assoc is (last ps) value))
+(defn- modify-is [is ps storeps value]
+  (comment println "modify-is " is ps storeps value)
+  (assoc is storeps value))
 
-(defn- remain-is [is ps value] 
-  (println "remain is " is ps value)
+(defn- remain-is [is ps storeps value] 
+  (comment println "remain is " is ps storeps value)
   is)
 
 (def ops {1 {:fnc + :params 3 :apply-fnc modify-is :store 1}
@@ -22,25 +24,27 @@
           4 {:fnc print-value :params 1 :apply-fnc remain-is :store 0}}) 
 
 (defn- get-param [is pc pm n]
-  (println "get-param " is " " pc " " pm " " n)
+  (comment println "get-param " is " " pc " " pm " " n)
   (case pm
     1 (nth is (+ pc n))
     0 (nth is (nth is (+ pc n)))))
 
 (defn- parse-opcode [is pc]
+  (comment println "parse-opcode <start> " is pc)
   (let [opn (mod (nth is pc) 100)
         operation (ops opn)
         pms (for [i (range (- (operation :params) (operation :store)))] (mod (quot (nth is pc) (* 100 (exp 10 i))) 10))
         ps (for [i (range (- (operation :params) (operation :store)))] (get-param is (inc pc) (nth pms i) i))
-        storeps (if (zero? (operation :store() [] [(nth is (+ 1 pc (operation :params)))])]
-    [operation (concat ps [42] storeps)]))
+        storeps (if (zero? (operation :store)) nil (nth is (+ pc (operation :params))))]
+    (println ps storeps "---parse-opcode: " )
+    [operation ps storeps]))
 
 (defn step [is pc]
-  (let [[operation ps] (parse-opcode is pc)
+  (let [[operation ps storeps] (parse-opcode is pc)
         value (apply (operation :fnc) ps)
-        nis ((operation :apply-fnc) is ps value)]
-    (println "step: " value " / " is " -> " nis " with operation " operation " and ps " ps)
-    [((operation :apply-fnc) is ps value) (operation :params)]))
+        nis ((operation :apply-fnc) is ps storeps value)]
+    (comment println "step: " value " / " is " -> " nis " with operation " operation " and ps " ps)
+    [((operation :apply-fnc) is ps storeps value) (operation :params)]))
 
 (defn run [iis]
   (loop [pc 0
@@ -48,6 +52,6 @@
     (if
       (= 99 (nth is pc)) (first is)
       (let [nis (step is pc)]
-        (println "nis is " nis)
+        (comment println "nis is " nis)
         (recur (+ pc (second nis) 1) (first nis) )))))
 
