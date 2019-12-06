936b897fd42d61b82da823829f26052d37e68eab
Sascha Koch <koch@smartsquare.de>
2019-12-06 08:02:55 +0100

    day6 part2

--- src/advent_of_code_2019/day06.clj
+++ src/advent_of_code_2019/day06.clj
@@ -2,14 +2,14 @@
 
 (defn all-orbits-for [os s]
   (loop [cs s
-      cnt 0]
+      akk []]
     (if (= "COM" cs)
-      cnt
-      (recur (os cs) (inc cnt)))))
+      akk
+      (recur (os cs) (conj akk (os cs))))))
 
 (defn all-orbits [os]
   (let [ks (into #{} (keys os))]
-    (apply + (map #(all-orbits-for os %) ks))))
+    (apply concat (map #(all-orbits-for os %) ks))))
 
 (defn orbit-map [l]
   (loop [[a & as] l
@@ -17,3 +17,15 @@
     (if (nil? a)
       akk
       (recur as (assoc akk (second a) (first a))))))
+
+(defn- intersect-list [c1 c2] 
+  (filter #(some #{%} c2) c1))
+
+(defn minimum-orbit-hops [os start end]
+  (let [aoy (all-orbits-for os start)
+        aos (all-orbits-for os end)
+        max-intersect (first (intersect-list aoy aos))
+        hops-y (.indexOf aoy max-intersect)
+        hops-s (.indexOf aos max-intersect)]
+    (+ hops-y hops-s)))
+
--- test/advent_of_code_2019/day06_test.clj
+++ test/advent_of_code_2019/day06_test.clj
@@ -7,12 +7,18 @@
 
 (deftest number-of-orbits
   (testing "the correct number of direct and indirect orbits"
-    (is (= 3 (all-orbits-for test-orbits "D")))
-    (is (= 7 (all-orbits-for test-orbits "L")))
-    (is (= 0 (all-orbits-for test-orbits "COM"))))
+    (is (= 3 (count (all-orbits-for test-orbits "D"))))
+    (is (= 7 (count (all-orbits-for test-orbits "L"))))
+    (is (= 0 (count (all-orbits-for test-orbits "COM")))))
   (testing "the correct number of all orbits"
-    (is (= 42 (all-orbits test-orbits)))))
+    (is (= 42 (count (all-orbits test-orbits))))))
   
+(def test-orbits-2 (orbit-map '(["COM" "B"] ["B" "C"] ["C" "D"] ["D" "E"] ["E" "F"] ["B" "G"] ["G" "H"] ["D" "I"] ["E" "J"] ["J" "K"] ["K" "L"] ["K" "YOU"] ["I" "SAN"])))
+
+(deftest orbit-hops-test
+  (testing "the amount of orbits hops between YOU and SAN"
+    (is (= 4 (minimum-orbit-hops test-orbits-2 "YOU" "SAN")))))
+
 
 ;
 ; the results
@@ -23,5 +29,6 @@
 
 (deftest result
   (testing "the correct answer for the given input for documentation purposes"
-    (is (= 160040 (all-orbits input)))))
+    (is (= 160040 (count (all-orbits input))))
+    (is (= -1 (minimum-orbit-hops input "YOU" "SAN")))))
 
