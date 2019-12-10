0c5edb931c82a7a86e5f0dea3b23c516c8f7968c
Sascha Koch <koch@smartsquare.de>
2019-12-10 16:55:36 +0100

    day10 wip

---
+++ src/advent_of_code_2019/day10.clj
@@ -0,0 +1,44 @@
+(ns advent-of-code-2019.day10)
+
+(defn- steigung [a1 a2]
+  (let [[x1 y1] a1
+        [x2 y2] a2]
+    (/ (- y2 y1) (- x2 x1))))
+  
+(defn- points-between [a1 a2]
+  (let [[[x1 y1] [x2 y2]] (sort-by first [a1 a2])]
+    (cond
+      (= x1 x2) (for [n (range (inc (min y1 y2)) (max y1 y2))] [x1 n])
+      (= y1 y2) (for [n (range (inc (min x1 x2)) (max x1 x2))] [n y1])
+      :otherwise (let [a (steigung a1 a2)]
+        (for [nx (range (inc x1) x2)
+              :let [ny (+ y1 (* a (- nx x1)))]
+              :when (and (integer? nx) (integer? ny))] 
+          [nx ny])))))
+
+(defn- asteroid? [p as]
+  (not (nil? (some #{p} as))))
+
+(defn- visible? [a1 a2 as]
+  (if (= a1 a2) 
+    false
+    (zero? (count (filter #(asteroid? % as) (points-between a1 a2))))))
+     
+(defn- visible-asteroids [a as]
+ (count (filter #(visible? a % as) as)))
+
+(defn max-asteroids [as]
+  (map #(vector % (visible-asteroids % as)) as))
+  
+(def example '([1 0] [4 0] [0 2] [1 2] [2 2] [3 2] [4 2] [4 3] [3 4] [4 4]))
+
+(comment sort-by second (max-asteroids (parse-lines (str/split (slurp "resources/day10/input.txt") #"\n"))))
+
+(max-asteroids (parse-lines (str/split (slurp "resources/day10/example1.txt") #"\n")))
+  
+(defn- parse-line [l y]
+  (map #(vector (first %) y) (filter #(= \# (second %)) (map-indexed (fn [i e] [i e]) l))))
+
+(defn parse-lines [lines]
+  (apply concat
+    (map-indexed (fn [i e] (parse-line e i)) lines)))
