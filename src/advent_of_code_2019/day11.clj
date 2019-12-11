e264fa8a28bc291bd6554af3bb198360d305fcaf
Sascha Koch <sascha.koch@web.de>
2019-12-11 21:34:44 +0100

    day11 refactoring

--- src/advent_of_code_2019/day11.clj
+++ src/advent_of_code_2019/day11.clj
@@ -2,19 +2,15 @@
   (:require [advent-of-code-2019.intcode :as intcode]))
 
 (defn- compute [initial-state color]
-  (comment println "compute " initial-state color)
   (loop [state (assoc initial-state :params [color])]
-    (comment println "compute.loop " state)
     (if (state :halt?)
       state
       (let [next-state (intcode/step state)]
-        (comment println "compute.loop.next-state " next-state)
         (if (= 2 (count (next-state :params)))
           next-state
           (recur next-state))))))
 
 (defn- rotate [rotation n]
-  (comment println "rotate: " n)
   (case rotation
     :UP (if (zero? n) :LEFT :RIGHT)
     :RIGHT (if (zero? n) :UP :DOWN)
@@ -22,7 +18,6 @@
     :LEFT (if (zero? n) :DOWN :UP)))
     
 (defn- new-pos [[x y] rotation]
-  (comment println "new-pos: " x y rotation)
   (case rotation
     :UP [x (inc y)]
     :RIGHT [(inc x) y]
@@ -32,24 +27,16 @@
 (defn- new-world [world position color]
   (case color
     1 (conj world position)
-    0 (remove #{position} world)
-    nil (do
-      (comment println "new-world: " world position color (count world))
-      world)))
+    0 (remove #{position} world)))
   
 (defn- update-on-output [[direction color] world position rotation]
-  (comment println "update-on-output: " color direction world position rotation)
   (let [new-rotation (rotate rotation direction)
         new-position (new-pos position new-rotation)
         new-wrld (new-world world position color)]
-    (comment println "uoo: " new-rotation new-position new-world)
-    (comment println "new-pos: " new-position)
     [new-wrld new-position new-rotation]))
 
 (defn- get-color [world position]
-  (if (some #{position} world)
-    1
-    0))
+  (if (some #{position} world) 1 0))
 
 (defn paint-robot [iis initial-world]
   (loop [state (intcode/create-state (into [] (concat iis (repeat 500 0))) [])
@@ -57,29 +44,30 @@
         visited #{}
         position [0 0]
         rotation :UP]
-    (if (state :halt?)
+  (let [color (get-color world position)
+        new-state (compute state color)]
+    (if (new-state :halt?)
       [(count visited) world] 
-      (let [color (get-color world position)
-            new-state (compute state color)
-            [nw np nr] (update-on-output (new-state :params) world position rotation)]
-        (recur new-state nw (conj visited np) np nr )))))
+      (let [[nw np nr] (update-on-output (new-state :params) world position rotation)]
+        (recur new-state nw (conj visited np) np nr))))))
 
 (defn part1 [iis]
-  (dec (first (paint-robot iis #{}))))
+  (first (paint-robot iis #{})))
   
 (defn- paint-line [world minx maxx]
    (apply str
      (for [x (range minx (inc maxx))]
-       (if (some #{x} (map first world)) "#" " "))))
-    
+       (if (some #{x} world) "#" " "))))
 
 (defn- paint-world [world]
-  (let [minx (apply min (map first world))
-        maxx (apply max (map first world))
-        maxy (apply max (map second world))
-        miny (apply min (map second world))]
-    (for [y (range maxy (dec miny) -1)]
-      (println (paint-line (filter #(= y (second %)) world) minx maxx)))))
+  (let [mfw (map first world)
+        msw (map second world)]
+    (for [y (range (apply max msw) (dec (apply min msw)) -1)]
+      (println 
+        (paint-line 
+          (map first (filter #(= y (second %)) world)) 
+          (apply min mfw)
+          (apply max mfw))))))
 
 (defn part2 [iip]
   (paint-world (second (paint-robot iip #{[0 0]}))))
